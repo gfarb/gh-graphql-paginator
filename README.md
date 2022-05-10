e9e97f55a5ff0c5479d53316a9f69b9e373ce2b1
Max Goldfarb <gfarb@github.com>
2022-05-10 14:41:14 -0400

    Init Release
    Initial release of gh-graphql-paginato.
    
    Simple GitHub GraphQL Paginator is a Node.js project that uses cursor-based pagination to paginate a GitHub GraphQL query Connection for single object.

---
+++ README.md
@@ -0,0 +1,78 @@
+# Simple GitHub GraphQL Paginator
+Simple GitHub GraphQL Paginator is a Node.js project that uses cursor-based pagination to paginate a GitHub GraphQL query Connection for single object.
+  
+## Installation
+Use the Node package manager [npm](https://www.npmjs.com/) to install Simple GitHub GraphQL Paginator.
+
+```bash
+npm install gh-graphql-paginator
+```
+
+## Usage
+#### Required:
+- Set an environment variable called `GITHUB_TOKEN` that stores a valid GitHub PAT that will be used for GitHub GraphQL API authentication.
+- The GraphQL query must include a variable called `endCursor` that is passed as the `after` argument value for the object which requires pagination. See the examples below.
+- Use Connections for the object you need to paginate, do not use Edges. See the examples below.
+- The Connection that requires pagination must also include `pageInfo` with `hasNextPage` and `endCursor` as well as `totalCount`. See the examples below.
+- The Connection that requires pagination must be a child of a single object. For example, this project cannot paginate issues and subsequently paginate comments for every issue. This project can paginate all issues pertaining to a repository or all issue comments for an issue.
+
+#### Examples:
+_This query is fetching public repositories from the `GitHub` organization. The query will be paginated and all public repositories from the `GitHub` organization will be returned_.
+```javascript
+import { paginate } from 'gh-graphql-paginator'
+const query = `
+  query ($endCursor: String) {
+    organization(login: "github") {
+      repositories(first: 100, after: $endCursor, privacy: PUBLIC) {
+        nodes {
+          id
+          name
+        }
+        totalCount
+        pageInfo {
+          hasNextPage
+          endCursor
+        }
+      }
+    }
+  }
+`;
+
+async function paginateQuery() {
+  const results = await paginate(query);
+  console.log(JSON.stringify(results));
+}
+```
+
+_This query is fetching issue comments from Issue #3 in the `github/github` repo. The query will be paginated and all issue comments for the issue will be returned_.
+```javascript
+import { paginate } from 'gh-graphql-paginator'
+const query = `
+  query ($endCursor: String) {
+  repository(name: "github", owner: "github") {
+    issue(number: 3) {
+      id
+      comments(first: 1, after: $endCursor) {
+        nodes {
+          id
+          body
+        }
+        totalCount
+        pageInfo {
+          hasNextPage
+          endCursor
+        }
+      }
+    }
+  }
+}
+```
+
+## License
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
+
+## Supporting Docs
+- [GraphQL Pagination](https://graphql.org/learn/pagination/)
+- [GitHub GraphQL API](https://docs.github.com/en/graphql)
+- [GitHub GraphQL Variables](https://docs.github.com/en/graphql/guides/forming-calls-with-graphql#working-with-variables)
+- [GitHub GraphQL Connections](https://docs.github.com/en/graphql/guides/introduction-to-graphql#connection)
---
+++ classes/QueryDataWrapper.js
@@ -0,0 +1,93 @@
+import _ from "lodash";
+import { graphql } from "@octokit/graphql";
+const graphqlWithAuth = graphql.defaults({
+  headers: {
+    authorization: `token ${process.env.GITHUB_TOKEN}`
+  }
+});
+
+export class QueryDataWrapper {
+  constructor(initQuery, initResponse) {
+    this.query = initQuery;
+    this.results = initResponse;
+    this.hasNextPage = true;
+    this.endCursor = undefined;
+    this.paginatedObjPath = undefined;
+    this.totalCountForObjToPaginate = undefined;
+    this.validQuery = undefined;
+    this.errors = [];
+    this.findObjectToPaginate(this.results);
+    this.validateQueryForPagination();
+  }
+
+  static async query(queryString, endCursor) {
+    if (endCursor === undefined) {
+      return await graphqlWithAuth(queryString);
+    } else {
+      return await graphqlWithAuth(queryString, { endCursor: endCursor });
+    }
+  }
+
+  static async paginateResults(dataWrapper) {
+    while (dataWrapper.hasNextPage) {
+      try {
+        const paginationResults = await QueryDataWrapper.query(dataWrapper.query, dataWrapper.endCursor);
+        dataWrapper.parsePaginationResponse(paginationResults);
+      } catch (error) {
+        dataWrapper.errors.push(error);
+        dataWrapper.hasNextPage === false;
+      }
+    }
+    _.get(dataWrapper.results, dataWrapper.paginatedObjPath).pageInfo.hasNextPage = dataWrapper.hasNextPage;
+    _.get(dataWrapper.results, dataWrapper.paginatedObjPath).pageInfo.endCursor = dataWrapper.endCursor;
+    if (dataWrapper.totalCountForObjToPaginate !== _.get(dataWrapper.results, dataWrapper.paginatedObjPath).nodes.length) {
+      dataWrapper.errors.push('Total records paginated does not equal total count for paginated object');
+    }
+  }
+
+  findObjectToPaginate(objToParse, parsedObjList) {
+    if (objToParse.hasOwnProperty('pageInfo')) {
+      this.hasNextPage = objToParse.pageInfo.hasNextPage;
+      this.endCursor = objToParse.pageInfo.endCursor;
+      this.paginatedObjPath = parsedObjList;
+      this.totalCountForObjToPaginate = objToParse.totalCount;
+      return;
+    }
+
+    const childObjectsToParse = new Map();
+    Object.keys(objToParse).forEach(function (key) {
+      if (typeof objToParse[key] === 'object' && objToParse[key] !== null) {
+        childObjectsToParse.set(key, objToParse[key])
+      }
+    });
+
+    childObjectsToParse.forEach((childObj, key) => {
+      const updatedParsedObjList = [];
+      parsedObjList === undefined ? updatedParsedObjList.push(key) : updatedParsedObjList.push(...parsedObjList, key);
+      this.findObjectToPaginate(childObj, updatedParsedObjList);
+    });
+  }
+
+  validateQueryForPagination() {
+    const variableRegEx = new RegExp(/\$endCursor:\s*String/, "g");
+    const cursorRegEx = new RegExp(/after:\s*\$endCursor/, "g");
+    if (this.paginatedObjPath === undefined || this.totalCountForObjToPaginate === undefined) {
+      this.errors.push('Query is not valid for pagination - Page Info &/or Total Count may be missing or incorrect');
+      this.validQuery = false;
+    } else if (this.hasNextPage === false) {
+      this.validQuery = false;
+    } else if (!variableRegEx.test(this.query) || !cursorRegEx.test(this.query)) {
+      this.validQuery = false;
+      this.errors.push('Query is not valid for pagination - Cursor variable may be missing or incorrect');
+    } else {
+      this.validQuery = true;
+    }
+  }
+
+  parsePaginationResponse(paginationResults) {
+    const paginatedResults = _.get(paginationResults, this.paginatedObjPath);
+    this.hasNextPage = paginatedResults.pageInfo.hasNextPage;
+    this.endCursor = paginatedResults.pageInfo.endCursor;
+    _.get(this.results, this.paginatedObjPath).nodes.push(...paginatedResults.nodes);
+  }
+}
