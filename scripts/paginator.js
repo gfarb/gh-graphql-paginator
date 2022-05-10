3c741bbd4ec64fffbb63329d8094d6d9a39481b9
gfarb <gfarb@github.com>
2022-05-10 12:35:09 -0400

    validations and assertions along with structural changes

---
+++ scripts/paginator.js
@@ -0,0 +1,9 @@
+import { QueryDataWrapper } from "../classes/QueryDataWrapper.js";
+
+export async function paginate(initQuery) {
+    const res = await QueryDataWrapper.query(initQuery);
+    const dataWrapper = new QueryDataWrapper(initQuery, res);
+    if(dataWrapper.validQuery) await QueryDataWrapper.paginateResults(dataWrapper);
+    dataWrapper.errors.forEach(error => console.error(error));
+    return dataWrapper.results;
+};
